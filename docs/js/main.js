bb2f58227251ff6c3a65ba32b12c9b72488833cf
Oceanos1 <michael.spierer@heig-vd.ch>
2017-12-08 23:03:40 +0100

    work with browsify

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
-require("d3");
+d3 = require("d3");
 require("./visualize.js");
 require("./custom.js");
-require("./graphs.js");
 require("./cloud/cloud.js");
 require("./cloud/d3.layout.cloud.js");
+require("./graphs.js");
