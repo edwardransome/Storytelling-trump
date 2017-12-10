ffc4539aaaea4c3d30c691d45fde0728c1a5b75e
Edward Ransome <edward.ransome@heig-vd.ch>
2017-12-10 19:15:59 +0100

    aldo

--- docs/js/bundle.js
+++ docs/js/bundle.js
@@ -400,79 +400,64 @@ var spirals = {
 };
 
 },{"d3-dispatch":6}],2:[function(require,module,exports){
-cloud = require("./index.js");
+cloud = require('./index.js');
 
 (function () {
-  var fill = d3v4.scaleOrdinal(d3v4.schemeCategory20),
-    w = 960,
+  const w = 960,
     h = 600,
-    dataWord = [],
-    csvPath = "data/wordoccurence.csv",
-    nbOccurrenceMax,
+    csvPath = 'data/wordoccurence.csv',
     nbMax = 250;
 
-  console.log("Ready to build cloud");
+  let fill = d3v4.scaleOrdinal(d3v4.schemeCategory20),
+    dataWord = [],
+    nbOccurrenceMax;
+
+  console.log('Ready to build cloud');
 
   function loadData(cb) {
-    console.log("Loading data via csv file");
-    d3v4.csv(csvPath, function (data) {
-
-      data.forEach(function (d) {
+    console.log('Loading data via csv file');
+    d3v4.csv(csvPath, (data) => {
+      data.forEach((d) => {
         if (dataWord.length < nbMax) {
-          dataWord.push({text: d.text, size: d.occurrence});
+          dataWord.push({ text: d.text, size: d.occurrence });
         }
       });
 
       nbOccurrenceMax = dataWord[0].size;
       console.log(nbOccurrenceMax);
-      console.log("We have " + dataWord.length + " words to put in cloud");
+      console.log(`We have ${dataWord.length} words to put in cloud`);
 
-      var layout = cloud()
+      const layout = cloud()
         .timeInterval(10)
         .size([w, h])
-        .words(dataWord.map(function (d) {
-          return {text: d.text, size: 10 + ((d.size / nbOccurrenceMax)*1.5) * 90};
-        }))
+        .words(dataWord.map(d => ({ text: d.text, size: 10 + ((d.size / nbOccurrenceMax) * 1.5) * 90 })))
         .padding(2)
-        .rotate(function () {
-          return (~~(Math.random() * 6) - 3) * 30;
-        })
-        .font("Impact")
-        .fontSize(function (d) {
-          return d.size;
-        })
-        .on("end", draw);
+        .rotate(() => (~~(Math.random() * 6) - 3) * 30)
+        .font('Impact')
+        .fontSize(d => d.size)
+        .on('end', draw);
 
-      console.log("Starting cloud layout");
+      console.log('Starting cloud layout');
       layout.start();
 
       function draw(words) {
-        d3v4.select("body").selectAll(".chart")
-          .attr("width", w)
-          .attr("height", h)
-          .append("g")
-          .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-          .selectAll("text")
+        d3v4.select('body').selectAll('.chart')
+          .attr('width', w)
+          .attr('height', h)
+          .append('g')
+          .attr('transform', `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`)
+          .selectAll('text')
           .data(words)
-          .enter().append("text")
-          .style("font-size", function (d) {
-            return d.size + "px";
-          })
-          .style("font-family", "Impact")
-          .style("fill", function (d, i) {
-            return fill(i);
-          })
-          .attr("text-anchor", "middle")
-          .attr("transform", function (d) {
-            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-          })
-          .text(function (d) {
-            return d.text;
-          });
+          .enter()
+          .append('text')
+          .style('font-size', d => `${d.size}px`)
+          .style('font-family', 'Impact')
+          .style('fill', (d, i) => fill(i))
+          .attr('text-anchor', 'middle')
+          .attr('transform', d => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
+          .text(d => d.text);
       }
-
     });
-
   }
 
 
@@ -482,16 +467,15 @@ cloud = require("./index.js");
       .await(tasksAreDone);
   }
 
-  doSequenceOfTasks(function (err, data) {
+  doSequenceOfTasks((err, data) => {
     if (err) {
-      console.log("Could not load data");
+      console.log('Could not load data');
       console.log(err);
     } else {
-      console.log("Cloud is created with");
+      console.log('Cloud is created with');
     }
   });
-
-})();
+}());
 
 },{"./index.js":1}],3:[function(require,module,exports){
 (function($) {
