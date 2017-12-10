2d15cadd47c1c0cf988000abc1e476bfbe385151
Edward Ransome <edward.ransome@heig-vd.ch>
2017-12-10 12:01:17 +0100

    stuff

--- docs/js/graphs.js
+++ docs/js/graphs.js
@@ -1,12 +1,9 @@
-var dc = require("dc");
-var crossfilter = require("crossfilter");
-
 var trumpChart = dc.lineChart("#dc-trump-chart", "chartGroup");
 var trumpDataTable = dc.dataTable("#data-table");
 
-d3v3.json('data/trumptwitterarchive.json', function (data) {
-    var dateFormat = d3v3.time.format('%a %b %d %H:%M:%S %Z %Y');
-    var numberFormat = d3v3.format('.2f');
+d3.json('data/trumptwitterarchive.json', function (data) {
+    var dateFormat = d3.time.format('%a %b %d %H:%M:%S %Z %Y');
+    var numberFormat = d3.format('.2f');
     data.forEach(function (d) {
         //console.log(d.created_at);
         d.created_at = dateFormat.parse(d.created_at);
@@ -17,11 +14,11 @@ d3v3.json('data/trumptwitterarchive.json', function (data) {
     var all = ndx.groupAll();
 
     // var yearlyDimension = ndx.dimension(function (d) {
-    //     return d3v3.time.year(d.created_at);
+    //     return d3.time.year(d.created_at);
     // });
 
     var monthDimension = ndx.dimension(function (d) {
-        return d3v3.time.month(d.created_at);
+        return d3.time.month(d.created_at);
     });
 
     var monthGroup = monthDimension.group()
@@ -35,7 +32,7 @@ d3v3.json('data/trumptwitterarchive.json', function (data) {
     trumpDataTable
         .dimension(dateDimension)
         .group(function (d){
-            var format=d3v3.format('02d');
+            var format=d3.format('02d');
             return d.created_at.getFullYear() + '-' + d.created_at.getMonth();
         })
         .size(10)
@@ -49,7 +46,7 @@ d3v3.json('data/trumptwitterarchive.json', function (data) {
         .sortBy(function(d){
             return d.created_at;
         })
-        .order(d3v3.ascending)
+        .order(d3.descending)
         .on('renderlet', function (table) {
             table.selectAll('.dc-table-group').classed('info', true);
         });
@@ -58,6 +55,7 @@ d3v3.json('data/trumptwitterarchive.json', function (data) {
         .dimension(ndx) // set dimension to all data
         .group(all); // set group to ndx.groupAll()
 
+    console.log("Starting chart configuration...");
     trumpChart
         .renderArea(true)
         .width(990)
@@ -66,18 +64,18 @@ d3v3.json('data/trumptwitterarchive.json', function (data) {
         .margins({top: 30, right: 50, bottom: 25, left: 40})
         .dimension(monthDimension)
         .group(function (d) {
-            var format = d3v3.format('02d');
+            var format = d3.format('02d');
             return d.created_at.getFullYear() + '-' + format((d.created_at.getMonth() + 1));
         })
         .mouseZoomable(true)
-        .x(d3v3.time.scale().domain([new Date(2009, 0, 1), new Date(2017, 12, 9)]))
-        .round(d3v3.time.month.round)
-        .xUnits(d3v3.time.months)
+        .x(d3.time.scale().domain(d3.extent(data, function(d) {
+            return d.created_at;
+        })))
+        .round(d3.time.month.round)
+        .xUnits(d3.time.months)
         .elasticY(true)
-        .renderHorizontalGridLines(true)
-
-
-
+        .renderHorizontalGridLines(true);
+        
     dc.renderAll();
     dc.redrawAll();
 
