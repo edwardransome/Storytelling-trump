6af982a5f2306ffcd12ec4b70e61071b086c2019
Edward Ransome <edward.ransome@heig-vd.ch>
2017-12-10 15:55:04 +0100

    coucou

--- docs/js/graphs.js
+++ docs/js/graphs.js
@@ -1,9 +1,10 @@
 var trumpDataTable = dc.dataTable("#data-table");
 var trumpChart = dc.lineChart("#dc-trump-chart");
+var dayOfWeekChart = dc.rowChart("#dc-dayweek-chart");
+var sourcePie = dc.pieChart("#dc-pie-chart");
 
 d3.json('data/trumptwitterarchive.json', function (data) {
     var dateFormat = d3.time.format('%a %b %d %H:%M:%S %Z %Y');
-    var numberFormat = d3.format('.2f');
     data.forEach(function (d) {
         //console.log(d.created_at);
         d.created_at = dateFormat.parse(d.created_at);
@@ -28,6 +29,41 @@ d3.json('data/trumptwitterarchive.json', function (data) {
     var dateDimension = ndx.dimension(function (d) {
         return d.created_at;
     });
+
+    var dayOfWeek = ndx.dimension(function(d) {
+      var day = d.created_at.getDay();
+      switch(day){
+      case 0: 
+        return "Sunday";
+      case 1:
+        return "Monday";
+      case 2:
+        return "Tuesday";
+      case 3:
+        return "Wednesday";
+      case 4:
+        return "Thursday";
+      case 5:
+        return "Friday";
+      case 6:
+        return "Saturday";
+      }
+    });
+    var dayOfWeekGroup = dayOfWeek.group();
+    
+    var sourceDimension = ndx.dimension(function(d){
+      switch(d.source){
+        case "Twitter for Android":
+          return "Android";
+        case "Twitter Web Client":
+          return "Web Client";
+        case "Twitter for iPhone":
+          return "iPhone";
+        default:
+          return d.source;
+      }
+    });
+    var sourceGroup = sourceDimension.group();
 
     // data table
 
@@ -89,6 +125,28 @@ d3.json('data/trumptwitterarchive.json', function (data) {
         .xUnits(d3.time.months)
         .elasticY(true)
         .renderHorizontalGridLines(true);
+
+    dayOfWeekChart
+      .width(300)
+      .height(300)
+      .margins({top: 5, left: 10, right: 10, bottom: 20})
+      .dimension(dayOfWeek)
+      .group(dayOfWeekGroup)
+      .colors(d3.scale.category10())
+      .label(function(d){
+        return d.key;
+      })
+      .elasticX(true)
+      .xAxis().ticks(4);
+
+    sourcePie.width(300)
+    .height(300)
+    .radius(100)
+    .innerRadius(30)
+    .dimension(sourceDimension)
+    .title(function(d){return d.value;})
+    .group(sourceGroup);
+
 
     dc.renderAll();
     dc.redrawAll();
